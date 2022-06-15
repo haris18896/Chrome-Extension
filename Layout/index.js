16910289610a0120d32489c34ea9c8cc8bb21b02
haris96 <haris@fit-outfit.com>
2022-06-15 15:26:42 +0500

    working with servers list

--- Layout/index.js
+++ Layout/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { useRouter } from 'next/router'
 
 function Layout({ children }) {
+  const router = useRouter()
   return (
     <div className='Layout'>
-      <Header />
+      {['/connect/connect'].includes(router.pathname) ? <Header /> : ''}
       {children}
       <Footer />
     </div>
--- pages/servers/servers-list.js
+++ pages/servers/servers-list.js
@@ -1,11 +1,16 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import Layout from '../../Layout'
 
 function Servers() {
+  const router = useRouter()
+
   return (
-    <Layout>
-      <div>Servers</div>
-    </Layout>
+    <div className='Servers'>
+      <div className='Servers--header'>a</div>
+      <div className='Servers--tabs'>b</div>
+      <div className='Servers--list'>c</div>
+    </div>
   )
 }
 
