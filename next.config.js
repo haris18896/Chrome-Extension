c54ed1708fc4beb7430a10482b938d099999001d
haris96 <haris@fit-outfit.com>
2022-06-16 14:28:45 +0500

    path and loader added to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    loader: 'cloudinary',
+    path: 'https://cloudinary.com/console/c-15229c9ad47c14596c201f153198d7/'
+  }
 }
 
 module.exports = nextConfig
