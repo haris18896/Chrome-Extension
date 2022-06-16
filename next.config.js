026cba32f32d6c77b7589b6c24b74581328d60e2
haris96 <haris@fit-outfit.com>
2022-06-16 13:17:07 +0500

    export added to main

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    loader: 'cloudinary',
-    path: 'https://cloudinary.com/console/c-15229c9ad47c14596c201f153198d7/'
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
