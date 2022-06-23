1cc9098fb0c22fcb0a4d917c78d2c6dcc89fb110
haris18896 <haris18896@gmail.com>
2022-06-23 19:38:03 +0500

    hydration problem solved and commented code removed

--- pages/index.js
+++ pages/index.js
@@ -73,32 +73,6 @@ function Connect() {
 
         <div className='Connect__Button'>
           <CountryServer />
-          {/* <div className='Connect__Button--container' onClick={() => router.push('/servers?amp=1')}>
-            {selectedServer && (
-              <>
-                <ReactCountryFlag
-                  as='div'
-                  countryCode={selectedServer?._id}
-                  style={{
-                    width: '2em',
-                    height: '2em',
-                    borderRadius: '50%',
-                  }}
-                  svg
-                  cdnUrl='https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/1x1/'
-                  cdnSuffix='svg'
-                  title='United States'
-                />
-                <p>{selectedServer?.name || 'Choose Country Server'}</p>
-              </>
-            )}
-
-            {isAmp ? (
-              <amp-img width='15' height='20' src='/assets/logos/rightArrow.svg' alt='rightArrow' layout='responsive' />
-            ) : (
-              <img width='15' height='20' src='/assets/logos/rightArrow.svg' alt='rightArrow' />
-            )}
-          </div> */}
           {connection ? (
             <div className='Connect__Button--ipAddress'>
               <p>IP Address: {ip || '00.00.00.00'}</p>
