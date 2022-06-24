c6e27682932b3cbd63a039af7507c288cc8eb54c
haris18896 <haris18896@gmail.com>
2022-06-24 16:02:16 +0500

    name of the content script in the background file has been changed to inject.js

--- build/background.js
+++ build/background.js
@@ -1,6 +1,13 @@
-let color = '#3aa757'
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color })
-  console.log('Default background color set to %cgreen', `color: ${color}`)
-})
+try {
+  //ON page change
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+      chrome.scripting.executeScript({
+        files: ['contentScript.js'],
+        target: { tabId: tab.id },
+      })
+    }
+  })
+} catch (e) {
+  console.log(e)
+}
