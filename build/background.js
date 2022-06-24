9c154d7ba81854abe7b340d70f500844e8b36013
haris18896 <haris18896@gmail.com>
2022-06-24 16:07:19 +0500

    this is working but its working in the extension

--- build/background.js
+++ build/background.js
@@ -3,7 +3,7 @@ try {
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
       chrome.scripting.executeScript({
-        files: ['contentScript.js'],
+        files: ['inject.js'],
         target: { tabId: tab.id },
       })
     }
