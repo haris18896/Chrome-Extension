4e7133ecfcb88105e3b566bfcf682295475eb468
haris18896 <haris18896@gmail.com>
2022-06-29 17:01:24 +0500

    injection alert is working now, but not console.log

--- public/backgroundScript.js
+++ public/backgroundScript.js
@@ -8,16 +8,16 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 })
 
 function checkUserLogin(request, sender, sendResponse) {
-  console.log('request', request, 'sender', sender, 'sendResponse', sendResponse)
-
   if (sender.tab?.url) {
     let cookieName = sender?.tab?.url
-    console.log('cookieName', cookieName, 'sender.tab.url', sender.tab.url)
+
     chrome.scripting.executeScript({
       target: { tabId: sender?.tab?.id },
       func: cookieInjection,
       args: [cookieName],
     })
+
+    console.log('cookieName', cookieName)
   }
 }
 
