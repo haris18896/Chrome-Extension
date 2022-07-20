e46b4b5489ba9312f702d4f56c7c9c50b1a8ae4d
haris18896 <haris18896@gmail.com>
2022-07-20 12:39:36 +0500

    proxy

--- public/backgroundScript.js
+++ public/backgroundScript.js
@@ -3,24 +3,24 @@
 // const ip = '222.139.221.185'
 // const port = '9091'
 
-// var config = {
-//   mode: 'fixed_servers',
-//   rules: {
-//     proxyForHttp: {
-//       scheme: 'socks5',
-//       host: '46.4.96.137',
-//       port: '1080',
-//     },
-//     bypassList: ['foobar.com'],
-//   },
-// }
-
 var config = {
-  mode: 'pac_script',
-  pacScript: {
-    data: 'function FindProxyForURL(url, host) { return PROXY 145.239.85.58:9300 }',
+  mode: 'fixed_servers',
+  rules: {
+    proxyForHttp: {
+      scheme: 'socks5',
+      host: '188.226.141.127',
+      port: '1080',
+    },
+    bypassList: ['foobar.com'],
   },
 }
+
+// var config = {
+//   mode: 'pac_script',
+//   pacScript: {
+//     data: 'function FindProxyForURL(url, host) { return PROXY 145.239.85.58:9300 }',
+//   },
+// }
 
 self.addEventListener('message', function (event) {
   self.clients.matchAll().then(all =>
