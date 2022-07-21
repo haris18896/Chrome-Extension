eb0b019fb468db0b23277ee96b547c864011bc88
haris18896 <haris18896@gmail.com>
2022-07-21 12:25:03 +0500

    :fire: removed getBrowserType from home page
    in index

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,11 @@ import dynamic from 'next/dynamic'
 import React from 'react'
 
 import Layout from '../Layout'
-import { getBrowserType } from '../lib/utils'
 
 export default function Connect() {
   const Home = dynamic(() => import('../components/Home/index'), {
     ssr: false,
   })
-
-  React.useEffect(() => {
-    getBrowserType()
-  }, [])
 
   return (
     <Layout navbar title='FriendsVPN Extension'>
