cbd0528055e36fea248012c69b2be7f04fdf9aca
haris18896 <haris18896@gmail.com>
2022-07-21 13:31:51 +0500

    :triangular_flag_on_post: Logger:
    added to utils

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,10 @@ import dynamic from 'next/dynamic'
 import React from 'react'
 
 import Layout from '../Layout'
+// import Logger from '../lib/logger'
 
 export default function Connect() {
+  // var logger = new Logger().getInstance()
   const Home = dynamic(() => import('../components/Home/index'), {
     ssr: false,
   })
